edf7b10c115dee22e887ac1369c8399d179f9c7d
siangi <siangi@gmx.ch>
2021-10-13 14:30:28 +0200

    facegenerator with grid and boring pixelblocks

--- day5/faceGenerator.js
+++ day5/faceGenerator.js
@@ -1,4 +1,9 @@
 let BACK_COLOR;
+// different color for the face Shape and the face itself
+// so you can see them better when they overlap
+let STROKE_COLOR;
+let FACE_STROKE_COLOR;
+const BASE_STROKE_WEIGHT = 10;
 
 let points = [];
 
@@ -8,33 +13,59 @@ let drawFunc;
 let clickFunc;
 
 function setup(){
-    drawFunc = polygonDraw;
+    frameRate(10);
+    drawFunc = quickDraw;
     clickFunc = setPolyPoint;
-    BACK_COLOR = color('#DEC891');
+    // BACK_COLOR = color('#DEC891');
+    BACK_COLOR = color(255);
+    STROKE_COLOR = color(80);
+    FACE_STROKE_COLOR = color(0);
+
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.mouseClicked(clickFunc);
-}
-
-function setUpButton(){
-    let button = createButton("generate a Face with this Polygon");
-    button.position(0,0);
-    button.mousePressed(generateFace);
 }
 
 function draw(){
     drawFunc();
 }
 
+function quickDraw(){
+    background(BACK_COLOR);
+    strokeWeight(BASE_STROKE_WEIGHT*2)
+    drawInstructions();
+    setPolyPoint();
+    redrawPoints(points);
+}
+
+// draw function while the user is drawing a Polygon
+// draws the lines from before, and to the mousePoint
+// needs to do this every frame, so we don't have 1000 lines to where the mouse
+// was before.
 function polygonDraw(){
     background(BACK_COLOR);
-    strokeWeight(10);
+
+    drawInstructions();
+
+    stroke(STROKE_COLOR);
+    strokeWeight(20);
     redrawPoints(points);
+    
     if (points.length >= 1){
         line(points[points.length - 1][0], points[points.length - 1][1], mouseX, mouseY);
     }
 }
 
-function redrawPoints(toDraw){
+function drawInstructions(){
+    push();
+    fill(0);
+    noStroke();
+    text("move mouse to start drawing a shape, enter to generate faces inside, backspace to start anew", 20, 20);
+    pop();
+    noFill();
+}
+
+//draws a polygon with the toDraw as cornerCoords. xDistance and yDistance are for drawing inside a grid
+function redrawPoints(toDraw, xDistance = 0, yDistance = 0){
     let pointBefore;
 
     toDraw.forEach(point => {
@@ -43,75 +74,134 @@ function redrawPoints(toDraw){
             return;
         }
 
-        line(pointBefore[0], pointBefore[1], point[0], point[1]);
+        line(pointBefore[0] + xDistance, pointBefore[1] + yDistance, point[0] + xDistance, point[1] + yDistance);
 
         pointBefore = point;
     });
 }
 
 function setPolyPoint(){
-    console.log("setPoint");
-    points.push([mouseX, mouseY]);
+    if(mouseX != 0 || mouseY != 0){
+        points.push([mouseX, mouseY]);
+    }
 }
 
-function generateFace(){
+function generateFaceGrid(){
+    TILE_FACTOR = 5;
+    tileWidth = windowWidth/TILE_FACTOR;
+    tileHeight = windowHeight/TILE_FACTOR;
+    scaledWeight = BASE_STROKE_WEIGHT/TILE_FACTOR;
+
     if (points.length <= 2){
         console.error("not enough points in the polygon to create a face!");
         return;
-    }  
-    
-    let left = findLeft(points);
-    let right = findRight(points);
-    let bottom = findBottom(points);
-    let top = findTop(points);
+    }
 
+    scaledPoints = [];
+    points.forEach(point =>{
+        scaledPoints.push([scaleDown(windowWidth, tileWidth, point[0]), scaleDown(windowHeight, tileHeight, point[1])]);
+    });
+
+    let left = findLeft(scaledPoints);
+    let right = findRight(scaledPoints);
+    let bottom = findBottom(scaledPoints);
+    let top = findTop(scaledPoints);
+
+    // find the reference points for the face features.
     let horizontalCenter = left + (right-left)/2;
     let topThird = top + (bottom-top)/3;
     let lowerThird = top + (bottom-top)*2/3;
     let quarterRight = left + (right-left)*3/4;
     let qaurterLeft = left + (right-left)/4
 
-    eyes(qaurterLeft, quarterRight, topThird, bottom-top);
-    nose(horizontalCenter, topThird, lowerThird, left-right);
-    mouth(horizontalCenter, lowerThird, right-left, bottom-top);
+    for(let row = 0; row < TILE_FACTOR; row++){
+        for(let col = 0; col < TILE_FACTOR; col++){
+            newTopLeftX = tileWidth*col;
+            newTopLeftY = tileHeight*row;
+            let boundaries = {
+                minX: newTopLeftX,
+                minY: newTopLeftY,
+                maxX: tileWidth*(col+1),
+                maxY: tileHeight*(row+1)
+            }
+
+            push();
+            stroke(STROKE_COLOR);
+            strokeWeight(scaledWeight*2);
+            redrawPoints(scaledPoints, newTopLeftX, newTopLeftY);
+
+            stroke(FACE_STROKE_COLOR);
+            strokeWeight(scaledWeight);
+
+            eyes(qaurterLeft + newTopLeftX, quarterRight + newTopLeftX, topThird + newTopLeftY, bottom-top, boundaries);
+            nose(horizontalCenter + newTopLeftX, topThird + newTopLeftY, lowerThird + newTopLeftY, left-right);
+            mouth(horizontalCenter + newTopLeftX, lowerThird + newTopLeftY, right-left, bottom-top, boundaries);
+            pop();
+        }   
+    }
 }
 
-function eyes(xLeft, xRight, y, faceHeight){
+function scaleDown(oldMax, newMax, value){
+    return (value/oldMax)*newMax;
+}
+
+
+
+// draw the eyes and eybrows. boundaries are for the curve() function so the eybrows dont go too crazy
+function eyes(xLeft, xRight, y, faceHeight, boundaries){
     let leftRadius = random(faceHeight/7, faceHeight/2)/2
     let rightRadius = randomGaussian(leftRadius, leftRadius/2);
     
+    // eyeball
+    push();
+    fill(BACK_COLOR);
     circle(xLeft, y, leftRadius);
     circle(xRight, y, rightRadius);
-    push();
+    pop();
 
+    // iris
+    push();
     noStroke();
     fill(0);
-    circle(xLeft, y, randomGaussian(leftRadius/4, leftRadius/8));
-    circle(xRight, y, randomGaussian(rightRadius/4, rightRadius/8));
+    circle(xLeft, y, randomGaussian(leftRadius/4, leftRadius/7));
+    circle(xRight, y, randomGaussian(rightRadius/4, rightRadius/7));
     pop(); 
+
+    // eyebrows, random curves with only the section above the lines visible
+    curve(random(boundaries.minX, boundaries.maxX), random(boundaries.minY, boundaries.maxY),
+        xLeft-leftRadius*1.2, y-leftRadius*1.2,
+        xLeft+leftRadius*1.2, y-leftRadius*1.2,
+        random(boundaries.minX, boundaries.maxX), random(boundaries.minY, boundaries.maxY));
+    
+    curve(random(boundaries.minX, boundaries.maxX), random(boundaries.minY, boundaries.maxY),
+        xRight-rightRadius*1.2, y-rightRadius*1.2,
+        xRight+rightRadius*1.2, y-rightRadius*1.2,
+        random(boundaries.minX, boundaries.maxX), random(boundaries.minY, boundaries.maxY));
 }
 
 function nose(xCenter, topThird, bottomThird, width){
-    let sd = (bottomThird - topThird)/ 4
-    topY = randomGaussian(topThird, sd)
-    bottomY = random(topThird, bottomThird-sd);
-    line(xCenter, randomGaussian(topThird, sd), xCenter, bottomY);
-    line(xCenter, bottomY, xCenter + random(width/10, width/5), randomGaussian(bottomY, sd/3));
+    let standardDeviation = (bottomThird - topThird)/ 4
+    topY = randomGaussian(topThird, standardDeviation)
+    bottomY = random(topThird, bottomThird-standardDeviation);
+    line(xCenter, randomGaussian(topThird, standardDeviation), xCenter, bottomY);
+    line(xCenter, bottomY, xCenter + random(width/10, width/5), randomGaussian(bottomY, standardDeviation/3));
 }
 
-function mouth(xCenter, bottomThird, width, height){
-    let sdHeight = height/10;
+function mouth(xCenter, bottomThird, width, height, boundaries){
+    let sdHeight = height/100;
     let maxWidth = width/3;
     let minWidth = width/8;
 
-    line(xCenter - random(minWidth, maxWidth), 
-        randomGaussian(bottomThird, sdHeight), 
-        xCenter + random(minWidth, maxWidth),
-        randomGaussian(bottomThird, sdHeight));
+    // again ranomd curve with ony a part of it visible
+    curve(random(boundaries.minX, boundaries.maxX), random(boundaries.minY, boundaries.maxY),
+        xCenter - random(minWidth, maxWidth), randomGaussian(bottomThird, sdHeight),
+        xCenter + random(minWidth, maxWidth), randomGaussian(bottomThird, sdHeight),
+        random(boundaries.minX, boundaries.maxX), random(boundaries.minY, boundaries.maxY))
 }
 
 
-// the draw loop function for the face painting. not used yet, maybe later
+// the draw loop function for the face painting. does not do anything, since
+// the face doens't need to be redrawn every frame.
 function faceDraw(){
     
 }
@@ -153,11 +243,21 @@ function findXPoint(toCheck, idxInElem, closeTo){
     return resultPoint;
 }
 
+// draws a face when enter is pressed
 function keyPressed(){
-    drawFunc = faceDraw;
-    background(BACK_COLOR);
-    redrawPoints(points);
-    generateFace();
+    if (keyCode == ENTER){
+        push();        
+        drawFunc = faceDraw;
+        background(BACK_COLOR);
+        drawInstructions();
+        // redrawPoints(points)
+        // generateFace();
+        generateFaceGrid();
+        pop();
+    } else if (keyCode == BACKSPACE){
+        points = [];
+        drawFunc = quickDraw;
+    }
 }
 
 
---
+++ day6/imageBlocks.js
@@ -0,0 +1,91 @@
+let base;
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    base.resize(0, windowHeight)
+    base.loadPixels();
+    result = averageBlocks(base, 500);
+    image(result, 0, 0);
+}
+
+function averageBlocks(img, noOfBlocks){
+    let size = Math.floor(Math.max(img.width, img.height)/15);
+    result = img;
+    let oldCoords = [];
+    
+        let x = 0;
+        let y = 0;
+
+    for(let i = 0; i < noOfBlocks; i++){
+        let coordsOK = true;
+
+        result.loadPixels();
+
+        do {      
+            x = Math.floor(random(0, img.width - size));
+            y = Math.floor(random(0, img.height - size));
+
+            coordsOK = true;
+            // oldCoords.forEach(coord => {
+            //     if ((x >= coord[0] && x <= coord[0] + size) || (y >= coord[1] && y <= coord[1] + size)){
+            //         coordsOK = false;
+            //         return;
+            //     } else {
+            //         coordsOK = true;
+            //     }
+            // });
+                
+        } while (!coordsOK);
+
+        result = averageBlock(x, y, size, size, result);
+        result.updatePixels();
+        oldCoords.push([x, y]);
+    }
+
+    return result;
+}
+
+function averageBlock(x, y, width, height, img){
+    let avgRed  = 0;
+    let avgGreen = 0;
+    let avgBlue = 0;
+    // one loop for getting the average Color and one Loop for setting it
+    for(let col = x; col <= x+width; col++){
+        for(let row = y; row <= y + height; row++){
+            // Calculate the pixel index * 4 because 4 color Channels
+            const index = (row * img.width + col) * 4;
+
+            // Sum the red, green, and blue values
+            avgRed += img.pixels[index + 0];
+            avgGreen += img.pixels[index + 1];
+            avgBlue += img.pixels[index + 2];
+        }
+    }
+    const numPixels = width*height;
+
+    avgRed /= numPixels;
+    avgGreen /= numPixels;
+    avgBlue /= numPixels;
+
+    for(let col = x; col <= x+width; col++){
+        for(let row = y; row <= y + height; row++){
+            // Calculate the pixel index
+            const index = (row * img.width + col) * 4;
+
+            // Sum the red, green, and blue values
+            img.pixels[index + 0] = avgRed;
+            img.pixels[index + 1] = avgGreen;
+            img.pixels[index + 2] = avgBlue;
+        }
+    }
+
+    return img;
+}
+
+function draw(){
+
+}
+
+function preload(){
+    base = loadImage('images/Berg.JPG');
+}
